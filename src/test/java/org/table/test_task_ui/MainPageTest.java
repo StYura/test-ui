844b10f22d7deadf57f47fdf20883cd802fa0598
automation <a@b.net>
2024-09-12 15:24:57 +0200

    simplify report

--- src/test/java/org/table/test_task_ui/MainPageTest.java
+++ src/test/java/org/table/test_task_ui/MainPageTest.java
@@ -1,5 +1,7 @@
 package org.table.test_task_ui;
 
+import org.assertj.core.api.Assertions;
+import org.assertj.core.internal.Failures;
 import org.table.test_task_ui.models.CompanyUIModel;
 import org.table.test_task_ui.page_objects.MainPage;
 import org.testng.annotations.BeforeMethod;
@@ -14,6 +16,8 @@ public class MainPageTest extends BaseTest {
 
     @BeforeMethod
     public void setUp() {
+        Assertions.setMaxStackTraceElementsDisplayed(10);
+        Failures.instance().setRemoveAssertJRelatedElementsFromStackTrace(true);
         open(getUrl());
     }
 
@@ -26,12 +30,13 @@ public class MainPageTest extends BaseTest {
         List<CompanyUIModel> table2Items = mainPage.getTableItems();
 
         assertThat(table1Items).hasSameSizeAs(table2Items);
-        assertThat(table1Items).allMatch(table1Item -> {
-            CompanyUIModel table2Item = table2Items
-                    .stream()
-                    .filter(table2item -> table2item.getCompanyName().equals(table1Item.getCompanyName()))
-                    .findFirst()
-                    .orElseThrow();
+        assertThat(table1Items).withFailMessage("Tables should have matching companies")
+                .allMatch(table1Item -> {
+                    CompanyUIModel table2Item = table2Items
+                            .stream()
+                            .filter(table2item -> table2item.getCompanyName().equals(table1Item.getCompanyName()))
+                            .findFirst()
+                            .orElseThrow();
             return table1Item.equalsCompany(table2Item);
         });
     }
